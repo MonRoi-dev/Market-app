0831b9f8a5f16aa2b570f2940727304bd5275107
David Arutiunya <monroi.dev@gmail.com>
2024-02-18 17:10:12 +0300

    created update product logic

--- controllers/productController.mjs
+++ controllers/productController.mjs
@@ -1,38 +1,53 @@
-import ProductModel from '../models/productModel.mjs'
+import ProductModel from '../models/productModel.mjs';
 
-class Product{
+class Product {
+	async createProduct(req, res) {
+		try {
+			const data = req.body;
+			const product = new ProductModel({
+				name: data.product_name,
+				info: data.product_info,
+				image: data.image_link || undefined,
+				price: data.product_price,
+			});
+			await product.save();
+			res.redirect('/');
+		} catch (err) {
+			res.status(500).json({ message: `Server Error: ${err}` });
+		}
+	}
 
-    async createProduct(req, res){
-        try{
-            const data = req.body
-            const product = new ProductModel({
-                name: data.product_name,
-                info: data.product_info,
-                image: data.image_link || undefined,
-                price: data.product_price
-            })
-            await product.save()
-            res.redirect('/')
-        }catch(err){
-            res.status(500).json({message: `Server Error: ${err}`})
-        }
-    }
+	async getProduct(req, res) {
+		try {
+			const id = req.params.id;
+			const product = await ProductModel.findById(id);
+			const data = {
+				name: product.name,
+				info: product.info,
+				price: product.price,
+				image: product.image,
+			};
+			res.render('product', { title: data.name, data });
+		} catch (err) {
+			res.status(500).json({ message: `Server Error: ${err}` });
+		}
+	}
 
-    async getProduct(req, res){
-        try{
-            const id = req.params.id
-            const product = await ProductModel.findById(id)
-            const data = {
-                name: product.name,
-                info: product.info,
-                price: product.price,
-                image: product.image
-            }
-            res.render('product', {title: data.name, data})
-        }catch(err){
-            res.status(500).json({message: `Server Error: ${err}`})
-        }
-    }
+	async updateProduct(req, res) {
+		try {
+			const data = req.body;
+            let dataToUpdate = {}
+            for (const [key, value] of Object.entries(data)) {
+                if(value){
+                    dataToUpdate[key] = value
+                }
+              }
+			await ProductModel.findByIdAndUpdate(dataToUpdate.id, dataToUpdate,{ new: true });
+			res.redirect('/');
+		} catch (err) {
+			res.status(500).json({ message: `Server Error: ${err}` });
+		}
+	}
 }
 
-export default new Product()
+export default new Product();
--- routes/adminRouter.mjs
+++ routes/adminRouter.mjs
@@ -4,5 +4,6 @@ import productController from '../controllers/productController.mjs'
 const router = Router()
 
 router.post('/create', productController.createProduct)
+router.post('/update', productController.updateProduct)
 
 export default router
